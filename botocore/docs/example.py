e6e4798eaa003c6da32a44bde8c87685d6433842
Zidaan Dutta <ziddutta@amazon.com>
2021-03-24 10:35:17 -0700

    Handling document types and adding C2J protocol tests

--- botocore/docs/example.py
+++ botocore/docs/example.py
@@ -11,7 +11,7 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 from botocore.docs.shape import ShapeDocumenter
-from botocore.docs.utils import py_default
+from botocore.docs.utils import py_default, DocumentedShape
 
 
 class BaseExampleDocumenter(ShapeDocumenter):
